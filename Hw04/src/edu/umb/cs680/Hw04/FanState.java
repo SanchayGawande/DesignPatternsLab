82c553386f2c5f0e9243d26bfbe7e92635e7a682
SanchayGawande <96362427+SanchayGawande@users.noreply.github.com>
2023-11-01 19:16:03 -0400

    Delete Hw04 directory

--- Hw04/src/edu/umb/cs680/Hw04/FanState.java
+++
@@ -1,9 +0,0 @@
-package edu.umb.cs680.Hw04;
-
-interface FanState {
-    void turnOn(FanContext fan);
-    void turnOff(FanContext fan);
-    void increaseSpeed(FanContext fan);
-    void decreaseSpeed(FanContext fan);
-    void toggleSpeed(FanContext fan);
-}
