9a802fdfdb49c0141f6ee7e017365775d411b04d
SanchayGawande <96362427+SanchayGawande@users.noreply.github.com>
2023-12-11 17:03:53 -0500

    Hw14

---
+++ Hw14/src/edu/umb/cs680/Hw14/Car.java
@@ -0,0 +1,75 @@
+package edu.umb.cs680.Hw14;
+import java.util.List;
+
+public class Car {
+        private String make;
+        private String model;
+        private int year;
+        private int mileage;
+        private float price;
+        private int dominationCount;
+
+        public Car(String make, String model, int year, int mileage, float price) {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.mileage= mileage;
+            this.price= price;
+        }
+
+
+    public String getMake() {
+            return make;
+        }
+
+        public String getModel() {
+            return model;
+        }
+
+        public int getYear() {
+            return year;
+        }
+
+        public int getMileage() {
+            return mileage;
+        }
+
+        public float getPrice(){
+            return price;
+        }
+
+    public int getDominationCount() {
+        return dominationCount;
+    }
+
+
+    public void setDominationCount(List<Car> cars) {
+        int count = 0;
+        for (Car otherCar : cars) {
+            if (!this.equals(otherCar) && otherCar.dominates(this)) {
+                count++;
+            }
+        }
+        this.dominationCount = count;
+    }
+
+    private boolean dominates(Car otherCar) {
+        boolean otherPriceBetterOrEqual = this.price <= otherCar.getPrice();
+        boolean otherYearBetterOrEqual = this.year >= otherCar.getYear();
+        boolean otherMileageBetterOrEqual = this.mileage <= otherCar.getMileage();
+
+        boolean atLeastOneAspectBetter =
+                (this.price < otherCar.getPrice()) ||
+                        (this.year > otherCar.getYear()) ||
+                        (this.mileage < otherCar.getMileage());
+
+        return otherPriceBetterOrEqual && otherYearBetterOrEqual && otherMileageBetterOrEqual && atLeastOneAspectBetter;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
