997daef7958b17836b659007492b89ba9330afa2
SanchayGawande <96362427+SanchayGawande@users.noreply.github.com>
2023-12-17 16:22:46 -0500

    Hw08

---
+++ Hw08/test/src/edu/umb/cs680/Hw08/FileTest.java
@@ -0,0 +1,129 @@
+package edu.umb.cs680.Hw08;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FileTest {
+
+    private static FileSystem fs;
+
+    @BeforeAll
+    public static void setUp() {
+        fs = testFixture.createFs();
+    }
+
+    private String[] FileToString(File f) {
+        String parentName = (f.getParent() != null) ? f.getParent().getName() : "null";
+        return new String[]{
+                f.getName(),
+                Integer.toString(f.getSize()),
+                parentName,
+                f.getCreationTime().toString()
+        };
+    }
+
+    @Test
+    public void verifyFileEqualityA_java() {
+        Directory root = fs.getRootDirs().getFirst();
+        Directory src = (Directory) root.getChildren().get(0);
+        File A_java = (File) src.getChildren().get(0);
+        String[] expected = {"A.java", "100", "src", "2021-04-22T11:00"};
+        assertArrayEquals(expected, FileToString(A_java));
+    }
+
+    @Test
+    public void verifyFileEqualityB_java() {
+        Directory root = fs.getRootDirs().getFirst();
+        Directory src = (Directory) root.getChildren().get(0);
+        File B_java = (File) src.getChildren().get(1);
+        String[] expected = {"B.java", "200", "src", "2021-04-23T11:00"};
+        assertArrayEquals(expected, FileToString(B_java));
+    }
+
+    @Test
+    public void verifyFileEqualityATest_java() {
+        Directory root = fs.getRootDirs().getFirst();
+        Directory test = (Directory) root.getChildren().get(1);
+        Directory testSrc = (Directory) test.getChildren().get(0);
+        File ATest_java = (File) testSrc.getChildren().get(0);
+        String[] expected = {"ATest.java", "250", "src", "2021-04-26T14:00"};
+        assertArrayEquals(expected, FileToString(ATest_java));
+    }
+
+    @Test
+    public void verifyFileEqualityBTest_java() {
+        Directory root = fs.getRootDirs().getFirst();
+        Directory test = (Directory) root.getChildren().get(1);
+        Directory testSrc = (Directory) test.getChildren().get(0);
+        File BTest_java = (File) testSrc.getChildren().get(1);
+        String[] expected = {"BTest.java", "300", "src", "2021-04-27T14:00"};
+        assertArrayEquals(expected, FileToString(BTest_java));
+    }
+
+    @Test
+    public void verifyFileEqualityReadme_md() {
+        Directory root = fs.getRootDirs().getFirst();
+        File readme_md = (File) root.getChildren().get(2);
+        String[] expected = {"readme.md", "50", "repo", "2021-04-28T15:00"};
+        assertArrayEquals(expected, FileToString(readme_md));
+    }
+
+
+    // Additional tests For Equality
+    @Test
+    public void verifyFileEqualityWithSameAttributes() {
+        LocalDateTime creationTime = LocalDateTime.of(2021, 4, 22, 11, 0);
+        Directory root = fs.getRootDirs().getFirst();
+        File NewFile1 = new File(root, "UMB", 100, creationTime);
+        File NewFile2 = new File(root, "UMB", 100, creationTime);
+        assertArrayEquals(FileToString(NewFile1), FileToString(NewFile2));
+    }
+
+    @Test
+    public void verifyFileEqualityWithSameParent() {
+        LocalDateTime creationTime = LocalDateTime.of(2021, 4, 24, 11, 0);
+        Directory root = fs.getRootDirs().getFirst();
+        Directory repo = new Directory(root, "repo", 0, creationTime);
+        File NewFile1 = new File(repo, "fileInrepo.txt", 200, creationTime);
+        File NewFile2 = new File(repo, "fileInrepo.txt", 200, creationTime);
+        assertArrayEquals(FileToString(NewFile1), FileToString(NewFile2));
+    }
+
+    @Test
+    public void verifyFileEqualityWithDifferentNames() {
+        Directory root = fs.getRootDirs().getFirst();
+        File NewFile1 = new File(root, "EDU", 100, LocalDateTime.of(2021, 4, 22, 11, 0));
+        File NewFile2 = new File(root, "UMB", 100, LocalDateTime.of(2021, 4, 22, 11, 0));
+        assertFalse(Arrays.equals(FileToString(NewFile1), FileToString(NewFile2)));
+    }
+
+    @Test
+    public void verifyFileEqualityWithDifferentSizes() {
+        Directory root = fs.getRootDirs().getFirst();
+        File NewFile1 = new File(root, "UMB", 100, LocalDateTime.of(2021, 4, 22, 11, 0));
+        File NewFile2 = new File(root, "UMB", 200, LocalDateTime.of(2021, 4, 22, 11, 0));
+        assertFalse(Arrays.equals(FileToString(NewFile1), FileToString(NewFile2)));
+    }
+
+    @Test
+    public void verifyFileEqualityWithDifferentParents() {
+        Directory root = fs.getRootDirs().getFirst();
+        Directory anotherDir = new Directory(root, "anotherDir", 0, LocalDateTime.now());
+        File NewFile1 = new File(root, "UMB", 100, LocalDateTime.of(2021, 4, 22, 11, 0));
+        File NewFile2 = new File(anotherDir, "UMB", 100, LocalDateTime.of(2021, 4, 22, 11, 0));
+        assertFalse(Arrays.equals(FileToString(NewFile1), FileToString(NewFile2)));
+    }
+
+    @Test
+    public void verifyFileEqualityWithDifferentCreationTimes() {
+        Directory root = fs.getRootDirs().getFirst();
+        File NewFile1 = new File(root, "UMB", 100, LocalDateTime.of(2021, 4, 22, 11, 0));
+        File NewFile2 = new File(root, "UMB", 100, LocalDateTime.of(2021, 4, 23, 11, 0));
+        assertFalse(Arrays.equals(FileToString(NewFile1), FileToString(NewFile2)));
+    }
+}
