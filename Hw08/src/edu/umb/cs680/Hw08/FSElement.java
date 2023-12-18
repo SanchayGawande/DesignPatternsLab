945a0dfead23d3ba49a6d2c891c5c141b5f7126f
SanchayGawande <96362427+SanchayGawande@users.noreply.github.com>
2023-12-17 20:07:12 -0500

    Hw08

---
+++ Hw08/src/edu/umb/cs680/Hw08/FSElement.java
@@ -0,0 +1,44 @@
+package edu.umb.cs680.Hw08;
+import java.time.LocalDateTime;
+
+public abstract class FSElement {
+    protected String name;
+    protected int size;
+    protected LocalDateTime creationTime;
+    protected Directory parent;
+
+    public FSElement(Directory parent, String name, int size, LocalDateTime creationTime) {
+        this.parent = parent;
+        this.name = name;
+        this.size = size;
+        this.creationTime = creationTime;
+    }
+
+    public Directory getParent() {
+        return this.parent;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return this.creationTime;
+    }
+
+    public abstract boolean isDirectory();
+
+    public abstract boolean isFile();
+}
