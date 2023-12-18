a9ff6734439f1ae1f5ba82eff9c0d18ea0125648
SanchayGawande <96362427+SanchayGawande@users.noreply.github.com>
2023-12-17 20:16:13 -0500

    Hw16

---
+++ Hw16/src/edu/umb/cs680/Hw16/WeatherData.java
@@ -0,0 +1,33 @@
+package edu.umb.cs680.Hw16;
+
+public class WeatherData {
+    private double temperature;
+    private double humidity;
+    private double pressure;
+
+    public WeatherData(double temperature, double humidity, double pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+
