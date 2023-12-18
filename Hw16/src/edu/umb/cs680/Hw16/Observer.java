4fb310d6640daf0e849b39e22be3c68cf1251194
SanchayGawande <96362427+SanchayGawande@users.noreply.github.com>
2023-12-17 20:15:08 -0500

    Delete Hw16 directory

--- Hw16/src/edu/umb/cs680/Hw16/Observer.java
+++
@@ -1,5 +0,0 @@
-package edu.umb.cs680.Hw16;
-
-public interface Observer<T> {
-    void update(Observable<T> sender, T event);
-}
--- Hw16/src/edu/umb/cs680/Hw16/WeatherDataObservable.java
+++
@@ -1,7 +0,0 @@
-package edu.umb.cs680.Hw16;
-
-public class WeatherDataObservable extends Observable<WeatherData> {
-    public void setWeatherData(double temperature, double humidity, double pressure) {
-        notifyObservers(new WeatherData(temperature, humidity, pressure));
-    }
-}
